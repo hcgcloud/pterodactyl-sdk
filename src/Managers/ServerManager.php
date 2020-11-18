ff9c072148ad03a624fdf26f94c88c0c32905d16
TLingC <my@tlingc.com>
2020-11-18 14:57:44 +0800

    Add server apis

--- src/Managers/ServerManager.php
+++ src/Managers/ServerManager.php
@@ -4,6 +4,7 @@ namespace HCGCloud\Pterodactyl\Managers;
 
 use HCGCloud\Pterodactyl\Resources\Collection;
 use HCGCloud\Pterodactyl\Resources\Server;
+use HCGCloud\Pterodactyl\Resources\Stats;
 
 class ServerManager extends Manager
 {
@@ -17,20 +18,29 @@ class ServerManager extends Manager
      */
     public function paginate(int $page = 1, array $query = [])
     {
-        return $this->http->get('servers', array_merge([
-            'page' => $page,
-        ], $query));
+        switch ($this->apiType) {
+            case 'application':
+                return $this->http->get('servers', array_merge([
+                    'page' => $page,
+                ], $query));
+            break;
+            case 'client':
+                return $this->http->get('', array_merge([
+                    'page' => $page,
+                ], $query));
+            break;
+        }
     }
 
     /**
      * Get a server instance by id.
      *
-     * @param int   $serverId
+     * @param mixed   $serverId
      * @param array $query
      *
      * @return Server
      */
-    public function get(int $serverId, array $query = [])
+    public function get($serverId, array $query = [])
     {
         return $this->http->get("servers/$serverId", $query);
     }
@@ -157,5 +167,55 @@ class ServerManager extends Manager
     public function forceDelete(int $serverId)
     {
         return $this->http->delete("servers/$serverId/force");
+    }
+
+    /**
+     * Get information for websocket console of a specified server.
+     *
+     * @param string $serverId
+     * 
+     * @return array
+     */
+    public function websocket(string $serverId)
+    {
+        return $this->http->get("servers/$serverId/websocket");
+    }
+
+    /**
+     * Get resource utilization of a specified server.
+     *
+     * @param string $serverId
+     * 
+     * @return Stats
+     */
+    public function resources(string $serverId)
+    {
+        return $this->http->get("servers/$serverId/resources");
+    }
+
+    /**
+     * Send a command to a specified server.
+     *
+     * @param string $serverId
+     * @param string $command
+     *
+     * @return array
+     */
+    public function command(string $serverId, string $command)
+    {
+        return $this->http->post("servers/$serverId/command", [], ['command' => $command]);
+    }
+
+    /**
+     * Send a power signal to a specified server.
+     *
+     * @param string $serverId
+     * @param string $signal
+     *
+     * @return array
+     */
+    public function power(string $serverId, string $signal)
+    {
+        return $this->http->post("servers/$serverId/power", [], ['signal' => $signal]);
     }
 }
